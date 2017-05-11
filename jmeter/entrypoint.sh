ccac145720b8ded0ef7f1c5288363629d654d953
Cody Crawford <me@codycrawford.net>
2017-05-10 23:22:56 -0500

    Update JMeter entrypoint to use etcd for REMOTE_HOSTS

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -14,32 +14,50 @@ then
 elif [[ ${ROLE} == "client" ]]
 then
     export TIME=$(/bin/date +%b-%d_%H-%M)
+
     if [[ -z ${PLAN+x} ]]
     then
 	export PLAN="test-plan.jmx"
     fi
-    export PLAN_FILE=/load-tests/${PLAN}
-    if [[ ! -e ${PLAN_FILE} ]]
+
+    export PLAN_FILE=/load_tests/${PLAN}
+
+    if [[ ! -e "${PLAN_FILE}" ]]
     then
 	echo "ERROR: Test plan file does not exist. Exiting..."
 	exit 1
     fi
-    if [[ ! -r ${PLAN_FILE} ]]
+
+    if [[ ! -r "${PLAN_FILE}" ]]
     then
 	echo "ERROR: Test plan file is not readable. Exiting..."
 	exit 1
     fi
-    if [[ -s ${PLAN_FILE} ]]
+
+    if [[ ! -s "${PLAN_FILE}" ]]
     then
 	echo "ERROR: Test plan file is empty. Exiting..."
 	exit 1
     fi
+
     cp ${PLAN_FILE} /load_tests/test-plan_${TIME}.jmx
+
+    for i in $(etcdctl --no-sync ls)
+    do
+	for j in $(etcdctl --no-sync ls ${i})
+	do
+	    z=$(etcdctl --no-sync get ${j})
+	    REMOTE_HOSTS="${REMOTE_HOSTS} ${i:1}:${z}"
+	done
+    done
+
     exec jmeter --nongui \
 	 -D "java.rmi.server.hostname=${OURIP}" \
 	 -D "client.rmi.localport=1099" \
 	 -t "/load_tests/test-plan_${TIME}.jmx" \
 	 -l "/load_tests/test-plan_${TIME}.jtl" \
-	 -R ${REMOTE_HOSTS}
-fi    
+	 -R "${REMOTE_HOSTS}"
+else
+    echo "ROLE not set, must specify client or server. Exiting..."
+    exit 1
 fi
